171726d0877cef603fd36982fe61e425519e66fd
Catrina Callahan <catrinacallahan@Catrinas-MacBook-Pro.local>
2020-04-06 16:42:36 -0500

    moved navbar to comp, changed app.tsx state

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,41 +1,35 @@
 import * as React from 'react';
-
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
 import FlashCard from './components/FlashCard';
+import NavBarHome from './Components/navbarHome';
 
 class App extends React.Component<IAppProps, IAppState> {
 	constructor(props: IAppProps) {
 		super(props);
 		this.state = {
-			name: null
+			navBar: 'home'
 		};
 	}
 
 	render() {
+		if(this.state.navBar === 'home'){
+			return <NavBarHome />
+		}
 		return (
 			<div>
-				<nav className="navbar navbar-expand-lg navbar-navbar bg-light">
-					<a className="navbar-brand" href="#">codeNewbies</a>
-					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon"></span></button>
-					<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-						<div className="navbar-nav">
-							<a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
-							<a className="nav-item nav-link justify-content-end" href="#">signIn / signUp</a>
-						</div>
-					</div>
-				</nav>
 				<FlashCard />
 			</div>
 		);
 	}
 }
 
-export interface IAppProps {
 
+export interface IAppProps {
+	
 }
 
 export interface IAppState {
-
+	navBar: string
 }
 
 export default App;
