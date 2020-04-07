f65b7fc63beaa9acb7f68d66c255140537215f72
Nicki Rakestraw <nickirakestraw@Nickis-MacBook-Air.local>
2020-04-07 16:53:21 -0500

    im bad at dis

--- src/client/Components/views/Login.tsx
+++ src/client/Components/views/Login.tsx
@@ -1,1 +1,18 @@
 import * as React from "react";
+
+
+class Login extends React.Component<LoginProps> {
+    render() { 
+        return ( 
+            <div>
+                <h1>Hello</h1>
+            </div>
+         );
+        }
+    }
+
+
+export interface LoginProps { }
+
+
+export default Login;
