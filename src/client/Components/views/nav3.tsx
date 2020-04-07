d7be1d765748cd87e000ca66836faacf3922781d
Catrina Callahan <catrinacallahan@Catrinas-MacBook-Pro.local>
2020-04-07 14:35:59 -0500

    navbars added

--- src/client/Components/views/nav3.tsx
+++ src/client/Components/views/nav3.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
 
 
-class NavBarHome extends React.Component<INavProps, INavState> {
+class NavBar3 extends React.Component<INavProps, INavState> {
     constructor(props: INavProps){
         super(props)
     }
 render(){
     return(
             <div>
-				<nav className="navbar navbar-expand-lg navbar-light bg-navBarColor align-items-end">
+				<nav className="navbar navbar-expand-lg navbar-light bg-navBarColor">
 					<a className="navbar-brand text-white" href="#">codeNewbies</a>
 					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 						<span className="navbar-toggler-icon"></span></button>
 					<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
 						<div className="navbar-nav">
-							<a className="nav-item nav-link text-white" href="#">signIn / signUp</a>
+							<a className="nav-item nav-link active text-white" href="#">Home <span className="sr-only">(current)</span></a>
+							<a className="nav-item nav-link justify-content-end text-white" href="#">newCatagory</a>
+							<a className="nav-item nav-link justify-content-end text-white" href="#">studyMore</a>
 						</div>
 					</div>
 				</nav>
@@ -33,4 +35,4 @@ interface INavState {
 }
 
 
-export default NavBarHome
+export default NavBar3
