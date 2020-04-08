f428bbe7315d612be25a0c4627b1249566fb46c1
Catrina Callahan <catrinacallahan@Catrinas-MacBook-Pro.local>
2020-04-08 14:30:51 -0500

    main styles and fonts added, home page created

--- src/client/views/Home.tsx
+++ src/client/views/Home.tsx
@@ -15,21 +15,35 @@ return(
     <React.Fragment>
         <NavBarHome />
         <div>
-            <h1 className = 'mainlogo ml-3 mt-2 mb-3' style = {{fontSize: '100px', color: '#D86410'}}>codeNewbie</h1>
+            <h1 className = 'mainlogo ml-3 mt-2 mb-3' style = {{fontSize: '100px', color: '#FFFFFF'}}>codeNewbie</h1>
             <div className = "d-flex flex-row justify-content-around my-3 pt-2">
-                <button className="btn btn-navbarcolor" style = {{width: '225px'}}>{techWord}</button>
-                <button className="btn btn-navbarcolor"style = {{width: '225px'}}>{studyWord}</button>
-                <button className="btn btn-navbarcolor"style = {{width: '225px'}}>{quizWord}</button>
-                <button className="btn btn-navbarcolor"style = {{width: '225px'}}>{resourcesWord}</button>
+                <button className="btn btn-darkBlue shadow-sm" style = {{width: '225px', fontSize : '20px', color: '#FFFFFF', textShadow: '1px 1px #D86410'}}>{techWord}</button>
+                <button className="btn btn-darkBlue shadow-sm"style = {{width: '225px', fontSize : '20px', color: '#FFFFFF', textShadow: '1px 1px #D86410'}}>{studyWord}</button>
+                <button className="btn btn-darkBlue shadow-sm"style = {{width: '225px', fontSize : '20px', color: '#FFFFFF', textShadow: '1px 1px #D86410'}}>{quizWord}</button>
+                <button className="btn btn-darkBlue shadow-sm"style = {{width: '225px', fontSize : '20px', color: '#FFFFFF', textShadow: '1px 1px #D86410'}}>{resourcesWord}</button>
             </div>
-            <div className = 'topbar bg-navbarcolor' style ={{height: '11px', width: 'auto'}}></div>
+            <div className = 'topbar bg-darkBlue' style ={{height: '11px', width: 'auto'}}></div>
         </div>
-        <div className = 'mt-5 text-center'>
+        <div className = 'mt-5 text-center' style = {{fontSize: '80px', color: '#FFFFFF'}}>
             <h2>{clickPhrase}</h2>
         </div>
-        <div className = "d-flex flex-row justify-content-around my-3 pt-2">
-            <div className=" d-flex circle bg-navbarcolor justify-content-center align-items-center" style = {{height: '200px', width: '200px', borderRadius: '50%'}}>front
-            <a className=" d-flex justify-content-center align-items-center">End</a></div>
+        <div className = "d-flex flex-row justify-content-center my-3 pt-2">
+            <div className=" d-flex flex-column m-3 circle bg-darkBlue justify-content-center align-items-center shadow-lg" style = {{height: '200px', width: '200px', borderRadius: '50%'}}>
+                <div className="topWord" style = {{fontSize: '36px', color: '#FFFFFF'}}>front</div>
+                <div className="bottomWord p-0 m-0" style = {{fontSize: '40px', color: '#c96567'}}>End</div>
+            </div>
+            <div className=" d-flex flex-column m-3 circle bg-darkBlue justify-content-center align-items-center shadow-lg" style = {{height: '200px', width: '200px', borderRadius: '50%'}}>
+                <div className="topWord" style = {{fontSize: '36px', color: '#FFFFFF'}}>back</div>
+                <div className="bottomWord p-0 m-0" style = {{fontSize: '40px', color: '#c96567'}}>End</div>
+            </div>
+            <div className=" d-flex flex-column m-3 circle bg-darkBlue justify-content-center align-items-center shadow-lg" style = {{height: '200px', width: '200px', borderRadius: '50%'}}>
+                <div className="topWord" style = {{fontSize: '36px', color: '#FFFFFF'}}>data</div>
+                <div className="bottomWord p-0 m-0" style = {{fontSize: '40px', color: '#c96567'}}>Base</div>
+            </div>
+            <div className=" d-flex flex-column m-3 circle bg-darkBlue justify-content-center align-items-center shadow-lg" style = {{height: '200px', width: '200px', borderRadius: '50%'}}>
+                <div className="topWord" style = {{fontSize: '36px', color: '#FFFFFF'}}>all</div>
+                <div className="bottomWord p-0 m-0" style = {{fontSize: '40px', color: '#c96567'}}>Terms</div>
+            </div>
             
         </div>
     </React.Fragment>
