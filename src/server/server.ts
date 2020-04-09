0b43a318fa14ff3f1b9cf8f7139f9ef96fadc2f6
Brandi English <brandienglish@Brandis-MacBook-Pro-2.local>
2020-04-08 20:03:46 -0500

    connected DB Queries and started postman routes and added .env to git ignore

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,13 +1,29 @@
 import * as express from 'express';
+ import * as morgan from 'morgan'
 import * as path from 'path';
+import { parse } from 'dotenv/types';
+import cardsRouter from './Routes'
+import usersRouter from './Routes'
+import config from './config '
+import routes from './routes'
+import * as passport from 'passport'
+
  
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(passport.initialize())
+app.use(morgan('dev'))
 app.use(express.json()); 
- 
+app.use('/api', cardsRouter)
+app.use('/api', usersRouter)
+app.use(routes)
 app.get("*", (req, res)=>  res.sendFile(path.join(__dirname, '../public/index.html')))
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server listening on port: ${port}`));
+parseInt('3000')
+
+app.listen(parseInt(config.port), () => console.log(`Server listening on port: ${parseInt(config.port)}`));
+
+import'./db/index';
+
