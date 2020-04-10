df25f1177534080003ff77a9dd1581129bc9d528
Catrina Callahan <catrinacallahan@Catrinas-MacBook-Pro.local>
2020-04-10 16:09:19 -0500

    made edits on userprofile

--- src/client/views/userProfile.tsx
+++ src/client/views/userProfile.tsx
@@ -1,30 +1,36 @@
 import * as React from "react";
 import  NavBar2 from "./Components/nav2";
+import { Link } from "react-router-dom";
 let userName = ' {username} '
 
 const User: React.FC<UserProps> = () => {
+    let username = '{ username }'
     return (
         <React.Fragment>
             <NavBar2 />
             
       <div>
-      <div className="width:100px; height:100px; bg-darkBlue m-2 rounded border-white text-center" style={{fontSize : '60px'}}>
-      <h1 className = 'mainlogo justify-content-center ml-0 mt-0 mb-0' style = {{height: '245px', color: '#FFFFFF'}}>{ userName }</h1>
+      <div className="bg-darkBlue m-2 rounded text-center container-fluid mt-4">
+          <div className="row d-flex flex-column">
+            {/* this will be generated form DB */}
+            <h1 className = 'justify-content-center mt-2' style = {{fontSize : '60px', color: '#FFFFFF'}}>{username}</h1>
+            <h2 className = 'justify-content-center' style = {{fontSize : '25px', color: '#97AABD'}}>Difficulty: Beginner</h2>
+          </div>
       </div>
-            <div className = 'topbar bg-darkBlue' style ={{ fontSize : '500px', height: '20px', width: 'auto'}}></div>
+            <div className = 'topbar bg-darkBlue mt-3' style ={{height: '20px', width: 'auto'}}></div>
             <div className = "d-flex flex-row justify-content-center my-3 pt-2">
-            <div className=" d-flex flex-column m-3 oval bg-darkBlue justify-content-center align-items-center shadow-lg" style = {{height: '300px', width: '400px', borderRadius: '90%', cursor: 'pointer'}}>
+            <Link className=" d-flex flex-column m-3 oval bg-darkBlue justify-content-center align-items-center shadow-lg" style = {{height: '300px', width: '300px', borderRadius: '90%', cursor: 'pointer'}} to='/study'>
                 <div className="topWord" style = {{fontSize: '36px', color: '#FFFFFF'}}>Study</div>
                 <div className="bottomWord p-0 m-0" style = {{fontSize: '40px', color: '#c96567'}}>Cards</div>
-            </div>
-            <div className=" d-flex flex-column m-3 oval bg-darkBlue justify-content-center align-items-center shadow-sm" style = {{height: '300px', width: '400px', borderRadius: '50%', cursor: 'pointer'}}>
+            </Link>
+            <Link className=" d-flex flex-column m-3 oval bg-darkBlue justify-content-center align-items-center shadow-sm" style = {{height: '300px', width: '300px', borderRadius: '50%', cursor: 'pointer'}} to='/review'>
                 <div className="topWord" style = {{fontSize: '36px', color: '#FFFFFF'}}>Review</div>
                 <div className="bottomWord p-0 m-0" style = {{fontSize: '40px', color: '#c96567'}}>Missed</div>
-            </div>
-            <div className=" d-flex flex-column m-3 oval bg-darkBlue justify-content-center align-items-center shadow-lg" style = {{height: '300px', width: '400px', borderRadius: '100%', cursor: 'pointer'}}>
+            </Link>
+            <Link className=" d-flex flex-column m-3 oval bg-darkBlue justify-content-center align-items-center shadow-lg" style = {{height: '300px', width: '300px', borderRadius: '100%', cursor: 'pointer'}} to='/quiz'>
                 <div className="topWord" style = {{fontSize: '36px', color: '#FFFFFF'}}>Quiz</div>
                 <div className="bottomWord p-0 m-0" style = {{fontSize: '40px', color: '#c96567'}}>Me</div>
-            </div>
+            </Link>
             
             </div>
         
