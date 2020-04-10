7e55dc28c0cd8c72c18d8cc0cba0a07b55df21e4
CalebDuke <dukecj18@gmail.com>
2020-04-10 09:09:32 -0500

    resource page

--- src/client/views/Login.tsx
+++ src/client/views/Login.tsx
@@ -6,59 +6,65 @@ const Login: React.FC<LoginProps> = () => {
         <React.Fragment>
             <NavBar2 />
             {/* SIGN IN */}
-            <span className="d-flex justify-content-center">
-                <h1 className='text-darkBlue' >signIn</h1>
-            </span>
+            <div className='row'>
+                <span className='shadow-lg col-sm-6'>
+                    <span className="d-flex justify-content-center">
+                        <h1 className='text-darkBlue' >signIn</h1>
+                    </span>
 
-            <span className="d-flex justify-content-center">
-                <form className="col-sm-4 form-group">
-                    <div className="form-group">
-                        <label className="text-darkBlue" htmlFor="inputUserName">userName</label>
-                        <input type="user" className="col-xs-2 form-control" />
-                    </div>
-                    <div className="form-group">
-                        <label className="text-darkBlue" htmlFor="inputPassword">password</label>
-                        <input type="password" className="form-control" />
-                    </div>
-                    <div className="d-flex justify-content-center">
-                        <button type="button" className="btn btn-darkRoseColor">Submit</button>
-                    </div>
-                </form>
-            </span>
+                    <span className="d-flex justify-content-center">
+                        <form className="col-sm-4 form-group">
+                            <div className="form-group">
+                                <label className="text-darkBlue" htmlFor="inputUserName">userName</label>
+                                <input type="user" className="shadow-lg col-xs-2 form-control" />
+                            </div>
+                            <div className="form-group">
+                                <label className="text-darkBlue" htmlFor="inputPassword">password</label>
+                                <input type="password" className="shadow-lg form-control" />
+                            </div>
+                            <div className="d-flex justify-content-center">
+                                <button type="button" className="shadow-lg btn btn-darkRoseColor">Submit</button>
+                            </div>
+                        </form>
+                    </span>
+                </span>
             {/* SIGNUP */}
-            <span className="d-flex justify-content-center">
-                <h1 className='text-darkBlue' >signUp</h1>
-            </span>
+                <span className='shadow-lg col-sm-6'>
+                    <span className="d-flex justify-content-center">
+                        <h1 className='text-darkBlue' >signUp</h1>
+                    </span>
 
-            <span className="d-flex justify-content-center">
-                <form className="col-sm-4 form-group">
-                    <div className="form-group">
-                        <label className='text-darkBlue' htmlFor="inputName">fullName</label>
-                        <input type="name" className="form-control" />
-                    </div>
-                    <div className="form-group">
-                        <label className='text-darkBlue' htmlFor="inputEmail">email</label>
-                        <input type="email" className="form-control" />
-                    </div>
-                    <div className="form-group">
-                        <label className='text-darkBlue' htmlFor="inputUserName">userName</label>
-                        <input type="user" className="form-control" />
-                    </div>
-                    <div className="form-group">
-                        <label className='text-darkBlue' htmlFor="inputPassword">password</label>
-                        <input type="password" className="form-control" />
-                    </div>
-                    <div className="form-group">
-                        <label className='text-darkBlue' htmlFor="inputPassword">confirmPassword</label>
-                        <input type="password" className="form-control" />
-                    </div>
-                    <div className="d-flex justify-content-center">
-                        <button type="button" className="btn btn-brightRoseColor">Submit</button>
-                    </div>
-                </form>
-            </span>
+                    <span className="d-flex justify-content-center">
+                        <form className="col-sm-4 form-group">
+                            <div className="form-group">
+                                <label className='text-darkBlue' htmlFor="inputName">fullName</label>
+                                <input type="name" className="shadow-lg form-control" />
+                            </div>
+                            <div className="form-group">
+                                <label className='text-darkBlue' htmlFor="inputEmail">email</label>
+                                <input type="email" className="shadow-lg form-control" />
+                            </div>
+                            <div className="form-group">
+                                <label className='text-darkBlue' htmlFor="inputUserName">userName</label>
+                                <input type="user" className="shadow-lg form-control" />
+                            </div>
+                            <div className="form-group">
+                                <label className='text-darkBlue' htmlFor="inputPassword">password</label>
+                                <input type="password" className="shadow-lg form-control" />
+                            </div>
+                            <div className="form-group">
+                                <label className='text-darkBlue' htmlFor="inputPassword">confirmPassword</label>
+                                <input type="password" className="shadow-lg form-control" />
+                            </div>
+                            <div className="d-flex justify-content-center">
+                                <button type="button" className="shadow-lg btn btn-brightRoseColor">Submit</button>
+                            </div>
+                        </form>
+                    </span>
+                </span>
+            </div>
 
- 
+
 
         </React.Fragment>
 
--- src/client/views/Resources.tsx
+++ src/client/views/Resources.tsx
@@ -1,16 +1,52 @@
 import * as React from "react";
+import NavBar2 from "./Components/nav2";
 
 const Resources: React.FC<ResourceProps> = () => {
-    return(
-        <div>
-            <h1> Resources Page(test) </h1>
-        </div>
+    return (
+        <React.Fragment>
+            <NavBar2 />
+            <span className="d-flex justify-content-center">
+                    <h1 className='text-darkBlue' >Resources</h1>
+                </span>
+            <span className="d-flex align-items-start flex-column ml-3 shadow">
+                <div className="d-flex flex-column bd-highlight ml-4 mb-3">
+                    <h2 className="p-2 text-darkblue">Resources Name</h2>
+                    <h4>
+                        <a href="#" className="shadow-sm text-greyColor">example link</a>
+                    </h4>
+                </div>
+                <div className="d-flex flex-column bd-highlight ml-4 mb-3">
+                    <h2 className="p-2 text-darkblue">Resources Name</h2>
+                    <h4>
+                        <a href="#" className="shadow-sm text-greyColor">example link</a>
+                    </h4>
+                </div>
+                <div className="d-flex flex-column bd-highlight ml-4 mb-3">
+                    <h2 className="p-2 text-darkblue">Resources Name</h2>
+                    <h4>
+                        <a href="#" className="shadow-sm text-greyColor">example link</a>
+                    </h4>
+                </div>
+                <div className="d-flex flex-column bd-highlight ml-4 mb-3">
+                    <h2 className="p-2 text-darkblue">Resources Name</h2>
+                    <h4>
+                        <a href="#" className="shadow-sm text-greyColor">example link</a>
+                    </h4>
+                </div>
+                <div className="d-flex flex-column bd-highlight ml-4 mb-3">
+                    <h2 className="p-2 text-darkblue">Resources Name</h2>
+                    <h4>
+                        <a href="#" className="shadow-sm text-greyColor">example link</a>
+                    </h4>
+                </div> 
+            </span>
+        </React.Fragment>
     )
-    
-    }
-    
-    
-    
-    
-        interface ResourceProps{}
-        export default Resources
+
+}
+
+
+
+
+interface ResourceProps { }
+export default Resources
