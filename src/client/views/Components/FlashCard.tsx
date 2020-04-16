130766829c832afd438e2db348ebe502d674996e
Brandi English <brandienglish@Brandis-MacBook-Pro-2.local>
2020-04-16 16:27:29 -0500

    for josh

--- src/client/views/Components/FlashCard.tsx
+++ src/client/views/Components/FlashCard.tsx
@@ -23,7 +23,16 @@ class FlashCard extends React.Component<IFlashCardProps, IFlashCardState > {
                         backgroundColor: "#f9e5e4"
                     }}>
                     <h3>Definition:</h3>
-                    <h1 className ='cardText'style={{ color: "314455" }}>{this.props.card.cardText}</h1>
+                    <h1 className ='cardText'style={{ color: "314455" }}>{
+                    this.props.card.cardText.split('\n').map(function(item, key) {
+                        console.log(item);
+                        return (
+                          <span key={key}>
+                            {item}
+                            <br/>
+                          </span>
+                        )
+                      })}</h1>
                         {/* This is where the content will go. The line above is a placeholder */}
                         {/* This needs a way to draw in from the database terms and definitions */}
                 </div>
